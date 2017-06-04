e4e6018f7173d37be7be07500016e9c50899794e
Artha Nugraha Jonar <takujeng@gmail.com>
2017-06-04 20:57:27 +0700

    Add files via upload

--- stars.py
+++ stars.py
@@ -1,7 +1,17 @@
-
-print ("how many stars row: ")
-stars_row = int(input("> "))
-for i in range(1,stars_row+1):
-    for j in range (1,i+1):
-        print ("*",end=" ")
-    print("")
+while True:
+    print ("how many stars row: ")
+    try:
+        stars_row = int(input("> "))
+    except:
+        print("it's not a number")
+        break
+    for i in range(1,stars_row+1):
+        for j in range (1,i+1):
+            print ("*",end=" ")
+        print("")
+    print("want to insert print star again? (input y or n)")
+    choose = input("> ")
+    if choose=="y":
+        continue
+    elif choose=="n":
+        break
